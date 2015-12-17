aa74f0bf13a986823bf8a0c69c50bf9199403448
helpfulrobot <chris@silverstripe.com>
2015-12-18 10:22:47 +1300

    Converted to PSR-2

--- code/DownloadLog.php
+++ code/DownloadLog.php
@@ -8,16 +8,16 @@
  */
 class DownloadLog extends DataObject
 {
-	private static $db = array(
-		'URL'       => 'Varchar(255)',      // logs url used (since file might be gone)
-	);
+    private static $db = array(
+        'URL'       => 'Varchar(255)',      // logs url used (since file might be gone)
+    );
 
-	private static $has_one = array(
-		'Order'     => 'Order',
-		'TempFile'  => 'DownloadTempFile',  // this may or may not still exist, but is good to have in case it does
-	);
+    private static $has_one = array(
+        'Order'     => 'Order',
+        'TempFile'  => 'DownloadTempFile',  // this may or may not still exist, but is good to have in case it does
+    );
 
-	private static $many_many = array(
-		'Files'     => 'File',
-	);
+    private static $many_many = array(
+        'Files'     => 'File',
+    );
 }
--- code/DownloadableAccountPageController.php
+++ code/DownloadableAccountPageController.php
@@ -9,68 +9,71 @@
  */
 class DownloadableAccountPageController extends Extension
 {
-	private static $allowed_actions = array('downloads');
+    private static $allowed_actions = array('downloads');
 
-	private static $sort_options = array(
-		'PurchaseDate'  => 'Date Purchased',
-		'Filename'      => 'File Name',
-		'Size'          => 'File Size',
-	);
+    private static $sort_options = array(
+        'PurchaseDate'  => 'Date Purchased',
+        'Filename'      => 'File Name',
+        'Size'          => 'File Size',
+    );
 
-	/**
-	 * This may need to be optimised. We'll just have to see how it performs.
-	 *
-	 * @param SS_HTTPRequest $req
-	 * @return array
-	 */
-	public function downloads(SS_HTTPRequest $req) {
-		$downloads = new ArrayList();
-		$member = Member::currentUser();
-		if (!$member || !$member->exists()) $this->httpError(401);
+    /**
+     * This may need to be optimised. We'll just have to see how it performs.
+     *
+     * @param SS_HTTPRequest $req
+     * @return array
+     */
+    public function downloads(SS_HTTPRequest $req)
+    {
+        $downloads = new ArrayList();
+        $member = Member::currentUser();
+        if (!$member || !$member->exists()) {
+            $this->httpError(401);
+        }
 
-		// create a dropdown for sorting
-		$sortOptions = Config::inst()->get('DownloadableAccountPageController', 'sort_options');
-		if ($sortOptions) {
-			$sort = $req->requestVar('sort');
+        // create a dropdown for sorting
+        $sortOptions = Config::inst()->get('DownloadableAccountPageController', 'sort_options');
+        if ($sortOptions) {
+            $sort = $req->requestVar('sort');
 
-			if (empty($sort)) {
-				reset($sortOptions);
-				$sort = key($sortOptions);
-			}
+            if (empty($sort)) {
+                reset($sortOptions);
+                $sort = key($sortOptions);
+            }
 
-			$sortControl = new DropdownField('download-sort', 'Sort By:', $sortOptions, $sort);
-		} else {
-			$sort = 'PurchaseDate';
-			$sortControl = '';
-		}
+            $sortControl = new DropdownField('download-sort', 'Sort By:', $sortOptions, $sort);
+        } else {
+            $sort = 'PurchaseDate';
+            $sortControl = '';
+        }
 
-		// create a list of downloads
-		$orders = $member->getPastOrders();
-		if (!empty($orders)) {
-			foreach ($orders as $order) {
-				if ($order->DownloadsAvailable()) {
-					$downloads->merge( $order->getDownloads() );
-				}
-			}
-		}
+        // create a list of downloads
+        $orders = $member->getPastOrders();
+        if (!empty($orders)) {
+            foreach ($orders as $order) {
+                if ($order->DownloadsAvailable()) {
+                    $downloads->merge($order->getDownloads());
+                }
+            }
+        }
 
-		Requirements::javascript(SHOP_DOWNLOADABLE_FOLDER . '/javascript/AccountPage_downloads.js');
+        Requirements::javascript(SHOP_DOWNLOADABLE_FOLDER . '/javascript/AccountPage_downloads.js');
 
-		return array(
-			'Title'         => 'Digital Purchases',
-			'Content'       => '',
-			'SortControl'   => $sortControl,
-			'HasDownloads'  => $downloads->count() > 0,
-			'Downloads'     => $downloads->sort($sort),
-		);
-	}
+        return array(
+            'Title'         => 'Digital Purchases',
+            'Content'       => '',
+            'SortControl'   => $sortControl,
+            'HasDownloads'  => $downloads->count() > 0,
+            'Downloads'     => $downloads->sort($sort),
+        );
+    }
 
-	
-	/**
-	 * @return string
-	 */
-	public function DownloadZipLink() {
-		return Config::inst()->get('Downloadable', 'download_link_base') . '/zip';
-	}
-
+    
+    /**
+     * @return string
+     */
+    public function DownloadZipLink()
+    {
+        return Config::inst()->get('Downloadable', 'download_link_base') . '/zip';
+    }
 }
